1e4a929822fc5205c5de8daaa37c6683f4e04f7b
Paulo Victor <pv.35@hotmail.com>
2025-08-11 14:49:37 +0100

    removed redundancy

--- srcs/requirements/mariadb/tools/mariadb-script.sh
+++ srcs/requirements/mariadb/tools/mariadb-script.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-# Use environment variables directly instead of secrets
-ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD"
-DB_PASSWORD="$MYSQL_PASSWORD"
-
 mkdir -p /run/mysqld /var/lib/mysql
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 chmod 755 /run/mysqld
@@ -24,13 +19,13 @@ if [ ! -f "/var/lib/mysql/mysql/user.MYD" ]; then
         DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost');
         DELETE FROM mysql.user WHERE user='';
         
-        CREATE USER 'root'@'%' IDENTIFIED BY '$ROOT_PASSWORD';
+        CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
         GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
         
         DROP DATABASE IF EXISTS test;
         
         CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-        CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+        CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
         GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
         
         FLUSH PRIVILEGES;
